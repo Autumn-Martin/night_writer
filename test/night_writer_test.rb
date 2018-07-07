8fbb771f52e3ed0599c2f1e1366671c66a6ceaee
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-06 18:31:00 -0600

    renames tests for it exists

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -4,13 +4,13 @@ require "pry"
 require "./lib/night_writer"
 
 class NightWriterTest < Minitest::Test
-  def test_it_exists
+  def test_night_writer_exists
     night_writer = NightWriter.new
     assert_instance_of NightWriter, night_writer
   end
 
-  def test_file_reader
-    file = FileReader.new
-    assert_instance_of FileReader, file
+  def test_file_reader_exists
+    file_reader = FileReader.new
+    assert_instance_of FileReader, file_reader
   end
 end
