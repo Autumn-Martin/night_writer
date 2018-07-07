77eb032f475e3ddf7348b596ebf15131b150f7be
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-06 18:11:57 -0600

    adds a test for instance of file reader

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -6,6 +6,11 @@ require "./lib/night_writer"
 class NightWriterTest < Minitest::Test
   def test_it_exists
     night_writer = NightWriter.new
-    assert_instance_of NightWriter, night_writer 
+    assert_instance_of NightWriter, night_writer
+  end
+
+  def test_file_reader
+    file = FileReader.new
+    assert_instance_of FileReader, file
   end
 end
