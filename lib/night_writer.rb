85c5dc4090653c3652f4fca837617d3630015b29
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-06 18:22:27 -0600

    adds class for file reader; adds attr reader, initialize method, & idea methods to NightWriter class

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,5 +1,24 @@
 require 'pry'
+class FileReader
+  def read
+    filename = ARGV[0]
+    File.read(filename)
+  end
+end
 
 class NightWriter
+  attr_reader :file_reader
 
-end 
+  def initialize
+    @reader = FileReader.new
+  end
+
+  def convert_file_to_braille
+    plain = reader.read
+    convert_to_braille(plain)
+  end
+
+  def convert_to_braille(input)
+
+  end
+end
