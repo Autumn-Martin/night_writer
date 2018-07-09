480beaa1cbdec740cf8df7f8b15cbf74e1863847
Harper Bellows <harper.bellows@gmail.com>
2018-07-09 08:43:23 -0600

    fix typo and remove extra #join causing file writing to error out

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -32,21 +32,14 @@ incoming_text = reader.read
 reader.close
 
 braille_converter = BrailleConverter.new
-outgoing_text = nw.output(incoming_text.strip)
-require "pry"; binding.pry
+outgoing_text = braille_converter.output_to_file(incoming_text.strip)
 writer = File.open(ARGV[1], "w")
-outgoing_text[0].each_index do |i|
-  writer.write(outgoing_text[0][i] <<"\n")
-  writer.write(outgoing_text[1][i] <<"\n")
-  writer.write(outgoing_text[2][i] <<"\n")
+  outgoing_text[0].each_index do |i|
+    writer.write(outgoing_text[0][i] << "\n")
+    writer.write(outgoing_text[1][i] << "\n")
+    writer.write(outgoing_text[2][i] << "\n")
 end
-# writer.write(outgoing_text)
 writer.close
-
-# outgoing_text = braille_converter.output_to_file(incoming_text.strip)
-# writer = File.open(ARGV[1], "w")
-# writer.write(outgoing_text)
-# writer.close
 
 character_number = (outgoing_text.length)
 puts "Created '#{ARGV[1]}' containing #{character_number} characters."
