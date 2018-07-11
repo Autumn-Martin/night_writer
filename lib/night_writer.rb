97f35d042287a7e5dc2530b915a6ab6d89ccd040
Harper Bellows <harper.bellows@gmail.com>
2018-07-11 09:36:13 -0600

    add small refactor write methods

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -29,12 +29,16 @@ require_relative "./braille_converter"
 
 reader = File.open(ARGV[0], "r")
 incoming_text = reader.read
+
+# require "pry"; binding.pry
+
 reader.close
 
 braille_converter = BrailleConverter.new
-outgoing_text = braille_converter.output_to_file(incoming_text.strip)
+outgoing_text = braille_converter.english_to_braille(incoming_text.strip)
 writer = File.open(ARGV[1], "w")
   outgoing_text[0].each_index do |i|
+    # require "pry"; binding.pry
     writer.write(outgoing_text[0][i] <<"\n")
     writer.write(outgoing_text[1][i] <<"\n")
     writer.write(outgoing_text[2][i] <<"\n")
