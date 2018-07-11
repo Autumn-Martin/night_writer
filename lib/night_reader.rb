0109157dc0446e9c47e3555d385a23d814545cfc
Harper Bellows <harper.bellows@gmail.com>
2018-07-11 09:35:35 -0600

    add small fix to reader writer

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,17 +1,12 @@
 require "./lib/english_converter"
 
-reader = File.open(ARGV[0], "r")
-incoming_text = reader.read
-reader.close
+night_reader = File.open(ARGV[0], "r")
+braille_text = night_reader.read.strip
+night_reader.close
 
 english_converter = EnglishConverter.new
-outgoing_text = english_converter.output_to_file(incoming_text.strip)
+outgoing_text = english_converter.braille_to_english(braille_text.strip)
 writer = File.open(ARGV[1], "w")
-#   outgoing_text[0].each_index do |i|
-#     writer.write(outgoing_text[0][i] <<"\n")
-#     writer.write(outgoing_text[1][i] <<"\n")
-#     writer.write(outgoing_text[2][i] <<"\n")
-# end
 writer.write(outgoing_text)
 writer.close
 
