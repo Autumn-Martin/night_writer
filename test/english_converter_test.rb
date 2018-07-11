7531201c0356bc7e43e69d6cab501d9953b904aa
Harper Bellows <harper.bellows@gmail.com>
2018-07-11 09:31:21 -0600

    add more testing coverage

--- test/english_converter_test.rb
+++ test/english_converter_test.rb
@@ -1,6 +1,5 @@
 require "minitest/autorun"
 require "minitest/pride"
-require 'pry'
 require "./lib/english_converter"
 
 class EnglishConverterTest < Minitest::Test
@@ -11,24 +10,69 @@ class EnglishConverterTest < Minitest::Test
     assert_instance_of EnglishConverter, english_converter
   end
 
-  def test_it_inverts_conversion_map
+  def test_it_can_split_incoming_english_text_into_an_array
+    # skip
     english_converter = EnglishConverter.new
 
-    assert_equal "a" ,  english_converter.invert[["0.", "..", ".."]]
+    incoming_text = "0.0.0.0.0.\n..........\n..........\n0.0.0.0.0.\n..........\n..........\n"
+
+    expected = ["0.0.0.0.0.","..........","..........","0.0.0.0.0.","..........",".........."]
+
+    assert_equal expected, english_converter.split_incoming_message(incoming_text)
   end
 
-  def test_convert_braille_to_plain_text
-    input_braille = [["0.", "..", ".."], ["0.", "0.", ".."], ["00", "..", ".."]]
+  def test_it_can_slice_incoming_text_array_into_equal_arrays
+    # skip
     english_converter = EnglishConverter.new
 
-    assert_equal "a", english_converter.convert_braille_to_plain_text([["0.", "..", ".."]])
-    assert_equal "abc", english_converter.convert_braille_to_plain_text(input_braille)
+    split = ["0.0.0.0.0.","..........","..........","0.0.0.0.0.",
+      "..........",".........."]
+
+    expected = ["0.0.0.0.0.","..........",".........."],
+    ["0.0.0.0.0.","..........",".........."]
+
+    assert_equal expected, english_converter.slice_message(split)
   end
 
-  def test_it_outputs_to_file
-    input_braille = [["0.", "..", ".."], ["0.", "0.", ".."], ["00", "..", ".."]]
+  def test_it_can_split_sliced_array_elements_to_two_character_strings
+    # skip
     english_converter = EnglishConverter.new
-    assert_equal "abc", english_converter.output_to_file(input_braille)
 
+    sliced = [["0.0.0.0.0.","..........",".........."],
+    ["0.0.0.0.0.","..........",".........."]]
+
+    expected = [[["0.","0.","0.", "0.", "0."], ["..","..","..","..",".."],
+    ["..","..","..","..",".."]], [["0.","0.","0.", "0.", "0."],
+    ["..","..","..","..",".."], ["..","..","..","..",".."]]]
+
+    assert_equal expected, english_converter.find_characters(sliced)
+  end
+
+  def test_it_can_move_braille_character_pairs_from_each_array_to_new_array
+    # skip
+    english_converter = EnglishConverter.new
+
+    scanned = [[["0.","0.","0.", "0.", "0."], ["..","..","..","..",".."],
+    ["..","..","..","..",".."]], [["00","00","00", "00", "00"],
+    ["00","00","00","00","00"], ["0.","0.","0.","0.","0."]]]
+    # "a" * 5, "q" * 5
+
+    expected = [[["0.", "..", ".."], ["0.", "..", ".."], ["0.", "..", ".."],
+    ["0.", "..", ".."], ["0.", "..", ".."]], [["00", "00", "0."],
+    ["00", "00", "0."], ["00", "00", "0."], ["00", "00", "0."],
+    ["00", "00", "0."]]]
+
+    assert_equal expected, english_converter.combine_braille_characters(scanned)
+  end
+
+  def test_it_can_translate_braille_to_english
+    # skip
+    english_converter = EnglishConverter.new
+    input = [[["0.", "..", ".."], ["0.", "..", ".."], ["0.", "..", ".."],
+    ["0.", "..", ".."], ["0.", "..", ".."]], [["00", "00", "0."],
+    ["00", "00", "0."], ["00", "00", "0."], ["00", "00", "0."],
+    ["00", "00", "0."]]]
+
+    assert_equal "aaaaaqqqqq", english_converter.convert_braille_letters_to_english_letters(input)
   end
 end
