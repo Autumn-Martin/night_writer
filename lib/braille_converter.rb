e97cad67d70feeddf68b385e581bb502060175d2
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-11 22:42:05 -0600

    minor changes

--- lib/braille_converter.rb
+++ lib/braille_converter.rb
@@ -7,23 +7,23 @@ class BrailleConverter
     @dictionary = Dictionary.new
   end
 
-  def english_to_braille(incoming_english_text)
-    convert_english_to_braille(incoming_english_text)
+  def english_to_braille(incoming_english)
+    convert_english_to_braille(incoming_english)
     @braille_output.map do |element|
      element.join.scan(/.{1,80}/) #array -join-> string ->scan -> arrays
     end
   end
   # scan can only works on strings, and returns arrays
 
-  def convert_english_to_braille(incoming_english_text) #runner method - calling all the methods within
-    converted = convert_text_letter_to_braille(incoming_english_text)
+  def convert_english_to_braille(incoming_english) #runner method - calling all the methods within
+    converted = convert_text_letter_to_braille(incoming_english)
     putting_elements_on_right_level(converted, 0) # top level
     putting_elements_on_right_level(converted, 1) # middle level
     putting_elements_on_right_level(converted, 2) # bottom level
   end
 
-  def convert_text_letter_to_braille(incoming_english_text)
-    incoming_english_text.chars.map do |letter| #chars will breakup the text into and array of individual letters
+  def convert_text_letter_to_braille(incoming_english)
+    incoming_english.chars.map do |letter| #chars will breakup the text into and array of individual letters
       @dictionary.conversion_map[letter] #letter = key, so accessing the value of key (returning the key's value = braille)
     end
   end
